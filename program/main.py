202431fe822c161c9704eec07c78e3d6aa310bab
Edvin Boul <kittenparry@users.noreply.github.com>
2020-07-29 22:31:33 +0300

    Switch to an OOP aproach
    Parent (left) and current (right) lists for one level above and current directories
    Start with current directory selected on parent list

--- program/main.py
+++ program/main.py
@@ -1,43 +1,85 @@
 import sys
 import os
 
-from PySide2.QtWidgets import QApplication, QLabel, QListWidget, QListWidgetItem
-from PySide2.QtGui import QIcon, QImage, QPixmap
+from PySide2.QtWidgets import QApplication, QWidget, QLabel, QListWidget, QListWidgetItem, QHBoxLayout, QVBoxLayout, QGroupBox
+from PySide2.QtGui import QIcon, QImage, QPixmap, QFont
 
 from icon import get_icon
 
-def main():
-	app = QApplication(sys.argv)
+class Window(QWidget):
+	def __init__(self):
+		super().__init__()
 
-	file_list = QListWidget()
-	files, dirs, dirpath = get_files()
-	print(dirs)
-	print(files)
-	print(dirpath)
+		self.setWindowTitle('file-browser')
+		self.setGeometry(200, 200, 640, 480)
 
-	dir_icons = []
-	file_icons = []
+		self.create_layout()
+		vbox = QVBoxLayout()
+		vbox.addWidget(self.group)
+		self.setLayout(vbox)
 
-	if len(files) > 0:
-		for i in range(len(files)):
-			files[i] = QListWidgetItem(QIcon(convert_to_icon(os.path.join(dirpath, files[i]))), files[i])
-	if len(dirs) > 0:
-		for i in range(len(dirs)):
-			dirs[i] = QListWidgetItem(QIcon(convert_to_icon(os.path.join(dirpath, dirs[i]))), dirs[i])
+		self.show()
 
-	if len(dirs) > 0:
-		for d in dirs:
-			file_list.addItem(d)
-	if len(files) > 0:
-		for f in files:
-			file_list.addItem(f)
-	
+	def create_layout(self):
+		path = 'F:\\_patreon\\browser-test'
+
+		self.group = QGroupBox(path)
+		self.group.setFont(QFont('Sanserif', 14))
+
+		hbox = QHBoxLayout()
+
+		try:
+			left_list = self.create_file_list(os.path.abspath(os.path.join(path, '..')), os.path.basename(path))
+		except Exception as e:
+			left_list = self.create_file_list('')
+			print(e)
+		left_list.setMinimumSize(100, 100)
+		hbox.addWidget(left_list)
+
+		try:
+			mid_list = self.create_file_list(path)
+		except Exception as e:
+			print(e)
+		mid_list.setMinimumSize(100, 100)
+		hbox.addWidget(mid_list)
+
+		self.group.setLayout(hbox)
+
+	def create_file_list(self, path, selected = None):
+		file_list = QListWidget(self)
+		if not path: # if empty
+			return file_list
+
+		files, dirs, dirpath = get_files(path)
+
+		dir_icons = []
+		file_icons = []
+
+		selected_index = -1
+		if len(files) > 0:
+			for i in range(len(files)):
+				files[i] = QListWidgetItem(QIcon(convert_to_icon(os.path.join(dirpath, files[i]))), files[i])
+		if len(dirs) > 0:
+			for i in range(len(dirs)):
+				if dirs[i] == selected:
+					selected_index = i
+				dirs[i] = QListWidgetItem(QIcon(convert_to_icon(os.path.join(dirpath, dirs[i]))), dirs[i])
+
+		if len(dirs) > 0:
+			for d in dirs:
+				file_list.addItem(d)
+		if len(files) > 0:
+			for f in files:
+				file_list.addItem(f)
+		
+		if selected_index >= 0:
+			file_list.item(selected_index).setSelected(True)
+
+		return file_list
+
+
 	# TODO: double click or right arrow action to go into that dir
 	# TODO: add a left list for parent, right list for child, check if those positions exist first
-
-	file_list.show()
-
-	app.exec_()
 
 def convert_to_icon(path):
 	'''Convert Image to QPixmap, which could be used in QIcon()
@@ -50,11 +92,11 @@ def convert_to_icon(path):
 	qim = QImage(data, 32, 32, QImage.Format_RGBA8888)
 	return QPixmap.fromImage(qim)
 
-def get_files():
+def get_files(path):
 	f = []
 	d = []
 	dp = ''
-	for (dirpath, dirnames, filenames) in os.walk('F:\\_patreon\\browser-test'):
+	for (dirpath, dirnames, filenames) in os.walk(path):
 		f.extend(filenames)
 		d.extend(dirnames)
 		dp = dirpath
@@ -63,4 +105,7 @@ def get_files():
 
 
 if __name__ == '__main__':
-	main()
+	app = QApplication(sys.argv)
+	window = Window()
+	app.exec_()
+	sys.exit()
